b8d09ebad5a2b155db740d93b87146e107c07a0b
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-07-26 13:18:51 +0600

    Project Update

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -5,19 +5,20 @@ import mision from '../../assets/mision.jpg'
 import vision from '../../assets/vision.jpg'
 
 import './AboutUs.css'
-import { FaChartLine } from 'react-icons/fa';
+import { FaChartLine, FaGlobe } from 'react-icons/fa';
+import { GrDocumentConfig } from 'react-icons/gr';
 
 const AboutUs = () => {
     return (
         <div className='min-h-screen'>
-            <div className='h-[60vh] flex justify-center items-center aboutImage bg-center bg-no-repeat bg-cover'>
+            <div className='lg:h-[60vh] h-[40vh] flex justify-center items-center aboutImage bg-center bg-no-repeat bg-cover'>
                 <h1 className='text-white text-5xl pb-3 border-b-4 border-[#20d7ab]'>About Us</h1>
             </div>
             {/* About Us Section */}
-            <div className='my-20 flex justify-center items-center space-x-10'>
+            <div className='my-20 flex flex-col lg:flex-row justify-center items-center lg:space-x-10 px-3 lg:px-0'>
                 <img className='w-[30rem] h-[50vh] rounded-lg' src={training} alt="" />
-                <div className='w-[40%] space-y-3'>
-                    <h1 className='text-4xl border-b-4 w-40 border-[#20d7ab]'>About Us</h1>
+                <div className='lg:w-[40%] text-center lg:text-left space-y-3 my-5'>
+                    <h1 className='text-4xl border-b-4 w-40 border-[#20d7ab] m-auto lg:m-0'>About Us</h1>
                     <h3 className='text-2xl'>
                         Welcome to
                         <span className='ms-2 text-[#20d7ab]'>MC Computer</span>
@@ -27,7 +28,7 @@ const AboutUs = () => {
                 </div>
             </div>
             {/* Mision and Plans Section */}
-            <div className='lg:mx-32 lg:my-20 flex justify-center items-center space-x-5'>
+            <div className='lg:mx-32 lg:my-20 flex flex-col lg:flex-row justify-center items-center lg:space-x-5 space-y-5 lg:space-y-0 px-3 pb-10'>
                 <div className='border flex flex-col justify-center items-center'>
                     <div className='w-full'>
                         <img className='w-full' src={plans} alt="" /> 
@@ -37,31 +38,31 @@ const AboutUs = () => {
                     </div>
                     <div className='p-3 text-center space-y-3'>
                         <h1 className='text-3xl font-bold text-[#20d7ab]'>Futures Plans</h1>
-                        <p>Our mission is to offer accessible, top-quality computer training for all learners.</p>
+                        <p className='text-[#000000c3]'>We plan to expand courses, develop online learning, and build industry partnerships for training.</p>
                     </div>
                 </div>
                 <div className='border flex flex-col justify-center items-center'>
                     <div className='w-full'>
-                        <img className='w-full' src={plans} alt="" /> 
+                        <img className='w-full' src={mision} alt="" /> 
                     </div>
                     <div className='-mt-10 flex justify-center items-center border-4 border-white shadow-lg bg-[#20d7ab]  text-white w-20 h-20 rounded-full text-4xl'>
-                        <FaChartLine />
+                        <FaGlobe />
                     </div>
                     <div className='p-3 text-center space-y-3'>
                         <h1 className='text-3xl font-bold text-[#20d7ab]'>Our Mission</h1>
-                        <p>Our mission is to offer accessible, top-quality computer training for all learners.</p>
+                        <p className='text-[#000000c3]'>Providing exceptional training and support to equip individuals with skills for a thriving digital future.</p>
                     </div>
                 </div>
                 <div className='border flex flex-col justify-center items-center'>
                     <div className='w-full'>
-                        <img className='w-full' src={plans} alt="" /> 
+                        <img className='w-full ' src={vision} alt="" /> 
                     </div>
                     <div className='-mt-10 flex justify-center items-center border-4 border-white shadow-lg bg-[#20d7ab]  text-white w-20 h-20 rounded-full text-4xl'>
-                        <FaChartLine />
+                        <GrDocumentConfig />
                     </div>
                     <div className='p-3 text-center space-y-3'>
                         <h1 className='text-3xl font-bold text-[#20d7ab]'>Our Vision</h1>
-                        <p>Our vision is to be the top center for empowering everyone with essential tech skills.</p>
+                        <p className='text-[#000000c3]'>Our vision is to be the top center for empowering everyone with essential tech skills.</p>
                     </div>
                 </div>
             </div>
